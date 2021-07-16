939473d7672efd2bd77d581389311d47206ed946
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-16 11:50:47 +0800

    Add counter app

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -8,191 +8,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz App',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.blue,
       ),
-      home: QuizPage(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class QuizPage extends StatefulWidget {
-  const QuizPage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  _QuizPageState createState() => _QuizPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _QuizPageState extends State<QuizPage> {
-  final questions = [
-    Question('Haris is handsome', true, 1),
-    Question('Flutter is easy', false, 2),
-    Question("I'm hungry", true, 3),
-  ];
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-  bool haveAnswered = false;
-  bool? chosenAnswer;
-  int currentQuestionIndex = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  void _resetCounter() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Are you sure to reset?'),
+          content: Text('All data reset cannot be restored.'),
+          actions: [
+            TextButton(
+              onPressed: Navigator.of(context).pop,
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() => _counter = 0);
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    Question currentQuestion = questions[currentQuestionIndex];
-
     return Scaffold(
-      body: Column(children: <Widget>[
-        QuizBody(question: currentQuestion, chosenAnswer: chosenAnswer),
-        if (haveAnswered)
-          AnswerButton(
-            onTap: () {
-              setState(() {
-                haveAnswered = false;
-                chosenAnswer = null;
-                currentQuestionIndex =
-                    (currentQuestionIndex + 1) % questions.length;
-              });
-            },
-            text: 'Next Question',
-            color: Colors.grey.shade800,
-          ),
-        if (!haveAnswered)
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                AnswerButton(
-                  onTap: () {
-                    setState(() {
-                      chosenAnswer = true;
-                      haveAnswered = true;
-                    });
-                  },
-                  text: 'True',
-                  color: Colors.green,
-                ),
-                AnswerButton(
-                  onTap: () {
-                    setState(() {
-                      chosenAnswer = false;
-                      haveAnswered = true;
-                    });
-                  },
-                  text: 'False',
-                  color: Colors.red,
-                ),
-              ],
-            ),
-          ),
-      ]),
-    );
-  }
-}
-
-class AnswerButton extends StatelessWidget {
-  const AnswerButton({
-    Key? key,
-    required this.onTap,
-    required this.text,
-    required this.color,
-  }) : super(key: key);
-
-  final Function() onTap;
-  final String text;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Ink(
-        color: color,
-        child: InkWell(
-          onTap: onTap,
-          child: Center(
-            child: Text(
-              text,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-    );
-  }
-}
-
-class QuizBody extends StatelessWidget {
-  final Question question;
-  const QuizBody({
-    Key? key,
-    required this.question,
-    required this.chosenAnswer,
-  }) : super(key: key);
-
-  final bool? chosenAnswer;
-
-  @override
-  Widget build(BuildContext context) {
-    final isCorrect = chosenAnswer != null && chosenAnswer == question.answer;
-    final isWrong = chosenAnswer != null && chosenAnswer != question.answer;
-    final youChooseText = "You chose $chosenAnswer. ";
-    return Expanded(
-      flex: 8,
-      child: Container(
-        padding: EdgeInsets.only(left: 20),
-        width: MediaQuery.of(context).size.width,
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          children: <Widget>[
             Text(
-              "Question #${question.id}",
-              style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey,
-              ),
+              'You have pushed the button this many times:',
             ),
-            SizedBox(height: 5),
-            Text(
-              question.text,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            AnimatedDefaultTextStyle(
+              child: Text(
+                '$_counter',
               ),
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4!
+                  .copyWith(fontSize: _counter.isOdd ? 150 : 34),
+              duration: Duration(milliseconds: 500),
             ),
-            SizedBox(height: 5),
-            if (isCorrect)
-              Text(
-                youChooseText + 'Correct!',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
-            if (isWrong)
-              Text(
-                youChooseText + 'Wrong!',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                ),
-              ),
           ],
         ),
       ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _resetCounter,
+            tooltip: 'Reset',
+            child: Icon(Icons.restart_alt),
+          ),
+        ],
+      ),
     );
   }
-}
-
-class Question {
-  final String text;
-  final bool answer;
-  final int id;
-
-  Question(this.text, this.answer, this.id);
 }
