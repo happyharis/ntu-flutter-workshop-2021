557b9484cf7d73689f0e6546e4dbdbbe2e2c546f
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-16 15:16:06 +0800

    Add first part of tutorial

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -25,130 +25,37 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  final questions = [
-    Question('Haris is handsome', true, 1),
-    Question('Flutter is easy', false, 2),
-    Question("I'm hungry", true, 3),
-  ];
-
-  bool haveAnswered = false;
-  bool? chosenAnswer;
-  int currentQuestionIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    Question currentQuestion = questions[currentQuestionIndex];
-
     return Scaffold(
       body: Column(children: <Widget>[
-        QuizBody(question: currentQuestion, chosenAnswer: chosenAnswer),
-        if (haveAnswered)
-          AnswerButton(
-            onTap: () {
-              setState(() {
-                haveAnswered = false;
-                chosenAnswer = null;
-                currentQuestionIndex =
-                    (currentQuestionIndex + 1) % questions.length;
-              });
-            },
-            text: 'Next Question',
-            color: Colors.grey.shade800,
-          ),
-        if (!haveAnswered)
-          Expanded(
-            flex: 1,
-            child: Row(
-              children: [
-                AnswerButton(
-                  onTap: () {
-                    setState(() {
-                      chosenAnswer = true;
-                      haveAnswered = true;
-                    });
-                  },
-                  text: 'True',
-                  color: Colors.green,
-                ),
-                AnswerButton(
-                  onTap: () {
-                    setState(() {
-                      chosenAnswer = false;
-                      haveAnswered = true;
-                    });
-                  },
-                  text: 'False',
-                  color: Colors.red,
-                ),
-              ],
-            ),
-          ),
+        QuizBody(),
+        Expanded(flex: 1, child: Container(color: Colors.amber)),
       ]),
     );
   }
 }
 
-class AnswerButton extends StatelessWidget {
-  const AnswerButton({
-    Key? key,
-    required this.onTap,
-    required this.text,
-    required this.color,
-  }) : super(key: key);
-
-  final Function() onTap;
-  final String text;
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Ink(
-        color: color,
-        child: InkWell(
-          onTap: onTap,
-          child: Center(
-            child: Text(
-              text,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class QuizBody extends StatelessWidget {
-  final Question question;
   const QuizBody({
     Key? key,
-    required this.question,
-    required this.chosenAnswer,
   }) : super(key: key);
-
-  final bool? chosenAnswer;
 
   @override
   Widget build(BuildContext context) {
-    final isCorrect = chosenAnswer != null && chosenAnswer == question.answer;
-    final isWrong = chosenAnswer != null && chosenAnswer != question.answer;
-    final youChooseText = "You chose $chosenAnswer. ";
+    final screenWidth = MediaQuery.of(context).size.width;
     return Expanded(
       flex: 8,
       child: Container(
         padding: EdgeInsets.only(left: 20),
-        width: MediaQuery.of(context).size.width,
+        width: screenWidth,
+        color: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          children: <Widget>[
             Text(
-              "Question #${question.id}",
+              "Question #1",
               style: TextStyle(
                 fontSize: 17,
                 fontWeight: FontWeight.bold,
@@ -157,42 +64,15 @@ class QuizBody extends StatelessWidget {
             ),
             SizedBox(height: 5),
             Text(
-              question.text,
+              'Haris is handsome.',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 5),
-            if (isCorrect)
-              Text(
-                youChooseText + 'Correct!',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.green,
-                ),
-              ),
-            if (isWrong)
-              Text(
-                youChooseText + 'Wrong!',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red,
-                ),
-              ),
           ],
         ),
       ),
     );
   }
-}
-
-class Question {
-  final String text;
-  final bool answer;
-  final int id;
-
-  Question(this.text, this.answer, this.id);
 }
