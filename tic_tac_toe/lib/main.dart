ec382aa2597d4e74011eb5cee00bdc0887cd0c6a
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-19 12:35:21 +0800

    Create grid view

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// import 'answer.dart';
 void main() {
   runApp(MyApp());
 }
@@ -12,107 +13,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TicTacToe(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class TicTacToe extends StatefulWidget {
+  const TicTacToe({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TicTacToeState createState() => _TicTacToeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _resetCounter() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Are you sure to reset?'),
-          content: Text('All data reset cannot be restored.'),
-          actions: [
-            TextButton(
-              onPressed: Navigator.of(context).pop,
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                setState(() => _counter = 0);
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
+class _TicTacToeState extends State<TicTacToe> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 3,
+          physics: NeverScrollableScrollPhysics(),
+          children: List.generate(
+            9,
+            (index) => OutlinedButton(
+              onPressed: () {},
+              child: Text('X'),
             ),
-            AnimatedDefaultTextStyle(
-              child: Text(
-                '$_counter',
-              ),
-              style: Theme.of(context)
-                  .textTheme
-                  .headline4!
-                  .copyWith(fontSize: _counter.isOdd ? 150 : 34),
-              duration: Duration(milliseconds: 500),
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: _resetCounter,
-            tooltip: 'Reset',
-            child: Icon(Icons.restart_alt),
-          ),
-        ],
       ),
     );
   }
