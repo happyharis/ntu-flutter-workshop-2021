83111f437e9688c43136ad1fd957c140a43f3734
Haris Samingan <muhamad_haris@hotmail.com>
2021-07-23 07:52:06 +0800

    Add validation in add screen

--- todo_app/lib/add_screen.dart
+++ todo_app/lib/add_screen.dart
@@ -10,47 +10,64 @@ class AddScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final titleController = TextEditingController();
     final descriptionController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Create task'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Title', style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: titleController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Complete todo app',
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Title', style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Title cannot be empty';
+                  }
+                },
+                controller: titleController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Complete todo app',
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Text('Description', style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: descriptionController,
-              maxLines: 10,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: hintText,
+              SizedBox(height: 20),
+              Text('Description',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: descriptionController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Description cannot be empty';
+                  }
+                },
+                maxLines: 10,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: hintText,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text('Submit'),
         icon: Icon(Icons.send),
         onPressed: () {
-          final title = titleController.text;
-          final description = descriptionController.text;
-          Provider.of<TaskNotifer>(context, listen: false)
-              .addTask(title, description);
-          Navigator.of(context).pop();
+          if (formKey.currentState!.validate()) {
+            final title = titleController.text;
+            final description = descriptionController.text;
+            Provider.of<TaskNotifer>(context, listen: false)
+                .addTask(title, description);
+            Navigator.of(context).pop();
+          }
         },
       ),
     );
